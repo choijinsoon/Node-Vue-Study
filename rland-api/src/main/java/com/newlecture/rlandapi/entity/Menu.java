f5739edaab70cd4676d2062e0d04c566651899ed
choijinsoon <wlstns2244@naver.com>
2023-01-18 12:43:42 +0900

    1:1 Tables Mapping

--- rland-api/src/main/java/com/newlecture/rlandapi/entity/Menu.java
+++ rland-api/src/main/java/com/newlecture/rlandapi/entity/Menu.java
@@ -1,23 +1,19 @@
 package com.newlecture.rlandapi.entity;
 
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Data
-// @Builder
-// @AllArgsConstructor
-// @NoArgsConstructor
 public class Menu {
 
   @Id
@@ -28,40 +24,8 @@ public class Menu {
   private String name;
   private int price;
 
-  // public Menu() {
-  // }
-  // public Menu(int id, String name, int price) {
-  //   this.id = id;
-  //   this.name = name;
-  //   this.price = price;
-  // }
-  
-  // public Menu(String name, int price) {
-  //   this.name = name;
-  //   this.price = price;
-  // }
-  // public int getId() {
-  //   return id;
-  // }
-  // public void setId(int id) {
-  //   this.id = id;
-  // }
-  // public String getName() {
-  //   return name;
-  // }
-  // public void setName(String name) {
-  //   this.name = name;
-  // }
-  // public int getPrice() {
-  //   return price;
-  // }
-  // public void setPrice(int price) {
-  //   this.price = price;
-  // }
-  // @Override
-  // public String toString() {
-  //   return "Menu [id=" + id + ", name=" + name + ", price=" + price + "]";
-  // }
-
+  @OneToOne
+  @JoinColumn(name = "id", referencedColumnName = "menuId")
+  private NutritionFacts nutritionFacts;
   
 }
--- rland-api/src/main/java/com/newlecture/rlandapi/entity/NutritionFacts.java
+++ rland-api/src/main/java/com/newlecture/rlandapi/entity/NutritionFacts.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,8 +19,10 @@ public class NutritionFacts {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private int calories;
-  
-  @Column(name = "saturated_fat")
   private int saturatedFat;
   private int menuId;
+
+  // @OneToOne
+  // @JoinColumn(name = "id")
+  // private Menu menu;
 }
