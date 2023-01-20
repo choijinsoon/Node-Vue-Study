3c48ae3780c250bf9a9d454b43b3ff120a602f6f
choijinsoon <wlstns2244@naver.com>
2023-01-20 13:11:21 +0900

    spring static 파일에 vue 파일 옮기기

--- rland-app/src/components/admin/Layout.vue
+++ rland-app/src/components/admin/Layout.vue
@@ -1,7 +1,8 @@
 <script setup>
-import Header from "./Header.vue";
-import Footer from "./Footer.vue";
+import Header from "../Header.vue";
+import Footer from "../Footer.vue";
 </script>
+
 <template>
     <Header />
     <router-view></router-view>
