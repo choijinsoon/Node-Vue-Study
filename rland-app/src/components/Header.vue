4a1789b1a7d40bec678ca01d01ada033175207fd
choijinsoon <wlstns2244@naver.com>
2023-01-31 10:48:49 +0900

    pinia를 이용하여 로그인 조건에 따른 헤더 출력

--- rland-app/src/components/Header.vue
+++ rland-app/src/components/Header.vue
@@ -1,7 +1,12 @@
 <script>
-
+import { useAuthStore } from '../stores/UserDetails';
 export default{
-    inject:['userDetails'],
+    setup(){
+        const store = useAuthStore();
+        return{
+            store
+        }
+    },
     methods:{
         logoutHandler(e){
             this.userDetails.username = null;
@@ -18,12 +23,12 @@ export default{
             <h1 class="header-title"><router-link to="/"><img class="logo" src="/image/logo-w.png"
                         alt="알랜드"></router-link></h1>
             <ul class="main-menu d-none d-inline-flex-sm">
-                <li><router-link to="/admin/menu/list"><span v-text="userDetails.username"></span> 카페메뉴관리</router-link></li>
+                <li><router-link to="/admin/menu/list"><span v-text="store.username"></span> 카페메뉴관리</router-link></li>
                 <li><router-link to="/menu/list">카페메뉴</router-link></li>
                 <li><router-link to="/">공지사항</router-link></li>
                 <li>
-                    <router-link v-if="userDetails.username == null" to="/user/login">로그인</router-link>
-                    <router-link v-if="userDetails.username != null" to="/user/logout" @click.prevent="logoutHandler">로그아웃</router-link>
+                    <router-link v-if="store.username == null" to="/user/login">로그인</router-link>
+                    <router-link v-if="store.username != null" to="/user/logout" @click.prevent="logoutHandler">로그아웃</router-link>
                 </li>
             </ul>
             <div class="d-none-sm"><a class="icon icon-menu icon-white" href="?m=on">메뉴버튼</a></div>
